44dd9f27882165b25dad4f4be8e305698163998f
Isaac Ayodele <ziggycodes25@gmail.com>
2023-02-20 15:11:47 +0100

    finished major parts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fancy: ['Raleway'],
+        
+      }
+    },
   },
   plugins: [],
 }
